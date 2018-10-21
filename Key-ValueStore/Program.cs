ea0f1181ee16d4529910d08ddc815fb63f16f4d9
Gabriel Khachatryan <gmkhach@gmail.com>
2018-10-20 21:44:45 -0400

    Add project files.

---
+++ Key-ValueStore/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Key_ValueStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
