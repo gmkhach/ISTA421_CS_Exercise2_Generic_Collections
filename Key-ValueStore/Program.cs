1b7bac99f5605809c0d85c708eb3acd4d3ffddf7
Gabriel Khachatryan <gmkhach@gmail.com>
2018-10-20 21:50:45 -0400

    struct KeyValue is implemented with two fields: string "key" and object "value"

--- Key-ValueStore/Program.cs
+++ Key-ValueStore/Program.cs
@@ -6,6 +6,18 @@ using System.Threading.Tasks;
 
 namespace Key_ValueStore
 {
+    struct KeyValue
+    {
+        public KeyValue(string key, object value)
+        {
+            this.key = key;
+            this.value = value;
+        }
+
+        public readonly string key;
+        public readonly object value;
+    }
+
     class Program
     {
         static void Main(string[] args)
