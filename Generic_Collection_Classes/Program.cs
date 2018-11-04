1e0632ae19e5866d2ba6421fc54a9a45c1f1d480
Gabriel Khachatryan <gmkhach@gmail.com>
2018-11-03 22:42:27 -0400

    instances of generic collections are instantiated in Main()

---
+++ Generic_Collection_Classes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generic_Collection_Classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> myList = new List<int>();
+            
+            LinkedList<string> myLinkedList = new LinkedList<string>();
+
+            Queue<char> myQueue = new Queue<char>();
+
+            Stack<double> myStack = new Stack<double>();
+
+            Dictionary<string, int> myDictionary = new Dictionary<string, int>();
+
+            SortedList<char, string> mySortedList = new SortedList<char, string>();
+
+            HashSet<bool> myHashSet = new HashSet<bool>();
+        }
+    }
+}
