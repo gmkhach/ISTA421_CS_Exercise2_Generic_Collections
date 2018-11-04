c756ebfe5012d910b1113e0a5656ffc1c1e0799a
Gabriel Khachatryan <gmkhach@gmail.com>
2018-11-03 23:04:32 -0400

    5 boolean values are pushed to myStack and printed after popping them right out

--- Generic_Collection_Classes/Program.cs
+++ Generic_Collection_Classes/Program.cs
@@ -9,7 +9,7 @@ namespace Generic_Collection_Classes
         {
             List<int> myList = new List<int>();
             for (int i = 1; i <= 5; i++) { myList.Add(i); }
-            PrintDescription("List");
+            PrintDescription("List<int>");
             foreach (var x in myList) { Console.WriteLine(x); }
 
             LinkedList<string> myLinkedList = new LinkedList<string>();
@@ -18,15 +18,18 @@ namespace Generic_Collection_Classes
             myLinkedList.AddLast("third");
             myLinkedList.AddLast("fourth");
             myLinkedList.AddLast("fifth");
-            PrintDescription("LinkedList");
+            PrintDescription("LinkedList<strign>");
             foreach (var x in myLinkedList) { Console.WriteLine(x); }
 
             Queue<char> myQueue = new Queue<char>();
             foreach (var ch in "abcdef") { myQueue.Enqueue(ch); }
-            PrintDescription("Queue");
+            PrintDescription("Queue<char>");
             foreach (var x in myQueue) { Console.WriteLine(x); }
 
-            Stack<double> myStack = new Stack<double>();
+            Stack<bool> myStack = new Stack<bool>();
+            for (int i = 1; i <= 5; i++) { myStack.Push(i % 2 == 0); }
+            PrintDescription("Stack<bool>");
+            foreach (var x in myStack) { Console.WriteLine(x); }
 
             Dictionary<string, int> myDictionary = new Dictionary<string, int>();
 
